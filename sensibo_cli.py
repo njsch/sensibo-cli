d98520a57fcc85e7ad4f5422504945ee5023db5a
Nathaniel Schmidt <schmidty2244@gmail.com>
2022-02-02 17:09:40 +1100

    Add Sensibo api template.

---
+++ sensibo_cli.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+    Sensibo-Cli: a means of controlling the hardware of your Sensibo Air or Sky by way of a command-line interface.
+    
+    @Author: Nathaniel Schmidt <schmidty2244@gmail.com>
+    Date created: 02/02/2022
+    Date modified: 
+    
+    License: MIT, as per The file in this repository or the Sensibo-python-sdk (https://github.com/Sensibo/sensibo-python-sdk/blob/master/LICENSE).
+"""
+
+import requests
+import json
+
+_SERVER = 'https://home.sensibo.com/api/v2'
+
+class SensiboClientAPI(object):
+    def __init__(self, api_key):
+        self._api_key = api_key
+
+    def _get(self, path, ** params):
+        params['apiKey'] = self._api_key
+        response = requests.get(_SERVER + path, params = params)
+        response.raise_for_status()
+        return response.json()
+
+    def _patch(self, path, data, ** params):
+        params['apiKey'] = self._api_key
+        response = requests.patch(_SERVER + path, params = params, data = data)
+        response.raise_for_status()
+        return response.json()
+
+    def devices(self):
+        result = self._get("/users/me/pods", fields="id,room")
+        return {x['room']['name']: x['id'] for x in result['result']}
+
+    def pod_measurement(self, podUid):
+        result = self._get("/pods/%s/measurements" % podUid)
+        return result['result']
+
+    def pod_ac_state(self, podUid):
+        result = self._get("/pods/%s/acStates" % podUid, limit = 1, fields="status,reason,acState")
+        return result['result'][0]['acState']
+
+    def pod_change_ac_state(self, podUid, currentAcState, propertyToChange, newValue):
+        self._patch("/pods/%s/acStates/%s" % (podUid, propertyToChange),
+                json.dumps({'currentAcState': currentAcState, 'newValue': newValue}))
