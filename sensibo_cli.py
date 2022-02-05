0f628d27250eeb98fcb90703a3630e90a6a922c4
Nathaniel Schmidt <schmidty2244@gmail.com>
2022-02-05 15:47:55 +1100

    ENHANCE: add interactive CLI interface. Add read and write functionality from and to CSV files to store API keys. Validate input and handle exceptions properly.

--- sensibo_cli.py
+++ sensibo_cli.py
@@ -53,9 +53,50 @@ class SensiboClientAPI:
                 json.dumps({'currentAcState': currentAcState, 'newValue': newValue}))
 
 # Frontend interactions:
-class App (SensiboClientAPI):
-    def __init__ (self, api_key = None):
-        super ().__init__(api_key)
+class App:
+    def __init__ (self):
+        self.filename = "api_keys.csv"
+        self.file = None
+        self.client = None
+        self.key = None
+        
+        # Look for API key and if not found, then ask for it (https://fedingo.com/python-reading-writing-to-same-file/, https://stackabuse.com/file-handling-in-python/, https://realpython.com/python-csv/, https://www.section.io/engineering-education/files-and-exceptions-in-python/)
+        while True:
+            try:
+                self.file = open (self.filename, 'r')
+                self.reader = csv.DictReader (self.file)
+                self.rows = 0
+                for self.row in self.reader:
+                    self.rows += 1
+                
+                if self.rows == 1:# We've only got one key stored, so let's just assume the user wants to use it, unless they specify otherwise
+                    for self.row in self.reader:
+                        self.key = self.reader['key']
+                
+                # else:
+                    # Do something
+                self.client = SensiboClientAPI (self.key)
+                break
+            except FileNotFoundError:
+                self.apiKeyQuery = self.valInput ("It appears that you have not yet provided the program with your Sensibo API key. At least one API key is required for the program to be able to run. Would you like to enter one in? (Type Y for yes     or N for no)")
+                while self.apiKeyQuery != "Y" and self.apiKeyQuery != "N":
+                    print ("Pleas type 'Y' or 'N'.")
+                    self.apiKeyQuery = self.valInput ("Want to enter the key in?")
+                
+                if self.apiKeyQuery == "Y":
+                    self.file = open (self.filename, 'w')
+                    self.keyName = input("What is the name for this key? You can use any name with letters, numbers or spaces, or specifically the name you entered into the Sensibo web app interface.")
+                    self.key = input ("And what is the actual API key? You can type it in directly, or paste it in from the website.")
+                    self.colHeads = ['name', 'key']
+                    self.writer = csv.DictWriter (self.file, fieldnames = self.colHeads)
+                    self.writer.writeheader ()
+                    self.writer.writerow ({self.colHeads[0] : self.keyName, self.colHeads[1] : self.key})
+                    self.file.close ()
+                    print ("Done, API key has been stored.")
+                else:
+                    print ("Okay. Quitting program.")
+                    exit ()
+                continue
         
         self.mainPrompt = "Sensibo-cli> "
         self.query = self.valInput (self.mainPrompt)
